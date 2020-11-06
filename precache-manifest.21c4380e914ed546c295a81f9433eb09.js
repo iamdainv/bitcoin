b2863c0f1b15a38442c73ab11be78d44eafb2b96
Quang Nguyen <quangnd.bhsoft@gmail.com>
2020-11-06 20:00:06 +0700

    Updates

---
+++ precache-manifest.21c4380e914ed546c295a81f9433eb09.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59ec449f78b60f87636ec09a9bfc8182",
+    "url": "/bitcoin/index.html"
+  },
+  {
+    "revision": "220da0f57a3a802ccb3c",
+    "url": "/bitcoin/static/css/2.cbe6aa73.chunk.css"
+  },
+  {
+    "revision": "52fb6526b06e2137adbd",
+    "url": "/bitcoin/static/css/main.1b799aa1.chunk.css"
+  },
+  {
+    "revision": "220da0f57a3a802ccb3c",
+    "url": "/bitcoin/static/js/2.4cefcc9d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/bitcoin/static/js/2.4cefcc9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52fb6526b06e2137adbd",
+    "url": "/bitcoin/static/js/main.3946678b.chunk.js"
+  },
+  {
+    "revision": "6f26f04dbede20c146b7",
+    "url": "/bitcoin/static/js/runtime-main.2bde909d.js"
+  },
+  {
+    "revision": "dd5ea50bdecd53e3f8eb38cd9c6c382a",
+    "url": "/bitcoin/static/media/coingecko-logo-d13d6bcceddbb003f146b33c2f7e8193d72b93bb343d38e392897c3df3e78bdd.dd5ea50b.webp"
+  }
+]);
